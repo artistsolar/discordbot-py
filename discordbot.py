54c76324f1fc91fee7c3fa7c1b42bb628060699b
artistsolar <119779808+artistsolar@users.noreply.github.com>
2023-04-20 15:54:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,39 +1,130 @@
 from cmath import log
 from distutils.sysconfig import PREFIX
 import discord
+from discord.ext import commands
+import requests
 from dotenv import load_dotenv
 import os
 load_dotenv()
+import requests
 
 PREFIX = os.environ['PREFIX']
 TOKEN = os.environ['TOKEN']
 
-client = discord.Client()
-bot = discord.Bot()
-
-# we need to limit the guilds for testing purposes
-
-# so other users wouldn't see the command that we're testing
-
-@bot.command(description="Sends the bot's latency.") # this decorator makes a slash command
-async def ping(ctx): # a slash command will be created with the name "ping"
-    await ctx.respond(f"Pong! Latency is {bot.latency}")
+client = commands.Bot(command_prefix= PREFIX)
+client.remove_command("help") #to remove the default boring help command
 
 @client.event
+
 async def on_ready():
-    print(f'Logged in as {client.user}.')
+    print("We have logged in as {0.user} ".format(client)) 
+    activity = discord.Game(name=".help", type=3)               # this is to writing prefix in playing a game.(optional)
+    await client.change_presence(status=discord.Status.online, activity=activity) # this is for making the status as an online and writing prefix in playing a game.(optional)  
+
+                            
+
+                            
+
+                            
+
+                            
+
+#Help commands
+
+@client.group(invoke_without_command=True)
+async def help(ctx):
+    embed = discord.Embed(title="IndianDesiMemer Help Center ✨",color=0xF49726)
+    embed.add_field(name="Command Categories :",value="🐸 `memes    :` Image generation with a memey twist.\n" + "🔧 `utility  :` Bot utility zone\n😏 `nsfw     :` Image generation with a memey twist.\n\nTo view the commands of a category, send `.help <category>`" ,inline=False)
+    embed.set_footer(icon_url=ctx.author.avatar_url,text="Help requested by: {}".format(ctx.author.display_name))
+    await ctx.send(embed=embed)
+
+                            
+
+#Sub-help command of memes
+
+@help.command ()
+async def memes(ctx):
+    embed=discord.Embed(title="IndianDesiMemer Help Center ✨", description="Commands of **meme** \n`.meme:`Memes",inline=False)
+    embed.set_footer(icon_url=ctx.author.avatar_url,text="Command requested by: {}".format(ctx.author.display_name))
+    await ctx.send(embed=embed)
+
+                            
+
+#Sub-help commands of nsfw                           
+
+@help.command ()
+async def nsfw(ctx) :
+    embed=discord.Embed(title="IndianDesiMemer Help Center ✨", description="Commands of **nsfw** \n`.nsfw:`NSFW", color=0xF49726)
+    embed.set_footer(icon_url=ctx.author.avatar_url,text="Command requested by: {}".format(ctx.author.display_name))
+    await ctx.send(embed=embed)
+
+#Sub-help commands of utility                           
+
+@help.command ()
+async def utility(ctx) :
+    embed=discord.Embed(title="IndianDesiMemer Help Center ✨", description="Commands of **utility** \n`.ping:`Latency", color=0xF49726)
+    embed.set_footer(icon_url=ctx.author.avatar_url,text="Command requested by: {}".format(ctx.author.display_name))
+    await ctx.send(embed=embed)                            
+
+# it is used for the cooldown to prevent the bot from spam attack                             
 
 @client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandOnCooldown):
+        await ctx.send("**Try after {0} second ".format(round(error.retry_after, 2)))                            
 
-    if message.content == f'{PREFIX}call':
-        await message.channel.send("callback!")
+                            
 
-    if message.content.startswith(f'{PREFIX}hello'):
-        await message.channel.send('Hello!')
+                            
 
+#meme command                            
+
+@client.command()
+@commands.cooldown(1, 10, commands.BucketType.channel) # it is used for the cooldown to prevent the bot from spam attack                            
+async def meme(ctx):
+    
+    response = requests.get("https://meme-api.herokuapp.com/gimme/"+"memes"+"memes"+"?t=all?hot")
+    
+    m = response.json()
+    postLink = (m["postLink"])
+    subreddit = (m["subreddit"])
+    title = (m["title"])
+    imageUrl =  (m["url"])
+    upVote = (m["ups"])
+    uv = str(upVote)
+    embed=discord.Embed(title= title, url=postLink,color=0xF49726)
+    embed.set_image(url=imageUrl)
+    embed.set_footer(text="\n👍\t"+ uv+ "  By :r/"+subreddit)
+    await ctx.send(embed=embed)                            
+
+                            
+
+#nsfw command
+
+@client.command()
+@commands.cooldown(1, 10, commands.BucketType.channel)
+async def nsfw(ctx):
+  if ctx.channel.is_nsfw():
+     print("nsfw work!!")
+  else:
+    print("You can use this command in a nsfw channel only !")                            
+
+                            
+
+                            
+
+#ping command                            
+
+@client.command()
+@commands.cooldown(1, 10, commands.BucketType.channel) # it is used for the cooldown to prevent the bot from spam attack                            
+async def ping(ctx):
+    await ctx.send('Ping! **{0}**ms'.format(round(client.latency, 1)))
+
+                            
+
+                            
+
+                            
 
 try:
     client.run(TOKEN)
