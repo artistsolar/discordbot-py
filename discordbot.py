2a9b71976e8b8aca2d7e7f82ef144014c565fefb
artistsolar <119779808+artistsolar@users.noreply.github.com>
2023-04-20 12:09:38 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,6 +9,15 @@ PREFIX = os.environ['PREFIX']
 TOKEN = os.environ['TOKEN']
 
 client = discord.Client()
+bot = discord.Bot()
+
+# we need to limit the guilds for testing purposes
+
+# so other users wouldn't see the command that we're testing
+
+@bot.command(description="Sends the bot's latency.") # this decorator makes a slash command
+async def ping(ctx): # a slash command will be created with the name "ping"
+    await ctx.respond(f"Pong! Latency is {bot.latency}")
 
 @client.event
 async def on_ready():
